0c450065653dad0d7430afbaa5a59e0375f97d1b
markihab <36458936+markihab@users.noreply.github.com>
2022-10-17 08:41:30 +0200

    Event Listeners

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,23 +1,37 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
-import dropdown.utils.WindowManager;
+import utils.EventReporter;
+import utils.WindowManager;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.EventListener;
 
 
 public class BaseTests {
     protected HomePage homePage;
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
 
     @BeforeClass
     public void setUp() {
 
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
         //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
         goHome();
 
@@ -32,6 +46,20 @@ public class BaseTests {
     @AfterClass
     public void tearDown() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            System.out.println("The  path of the screenshot : " + screenshot.getAbsolutePath());
+            try {
+                Files.move(screenshot, new File("resources/Screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public WindowManager getWindowManager() {
